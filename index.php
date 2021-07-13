2a7d59b0bcdbf3dc4c691e60fe3a42086ef66036
torque ninja <sayyidkhan92@hotmail.com>
2021-07-13 17:47:58 +0800

    add updated content

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <!-- navbar-->
     <header class="header">
       <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 py-lg-2">
-        <div class="container"><a class="navbar-brand py-3 d-flex align-items-center" href="index.html"><img src="img/logo.svg" alt="" width="30"><span class="text-uppercase text-small font-weight-bold text-dark ml-2 mb-0">Takoko</span></a>
+        <div class="container"><a class="navbar-brand py-3 d-flex align-items-center" href="index.php"><img src="img/logo.svg" alt="" width="30"><span class="text-uppercase text-small font-weight-bold text-dark ml-2 mb-0">Takoko</span></a>
           <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
